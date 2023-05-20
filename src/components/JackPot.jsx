3cf24f4b29ed50967dd08efd11fbe15dabdf4f1e
Ishwari18 <ishwarirele18@.com>
2023-05-20 17:09:30 +0530

    Initial

---
+++ src/components/JackPot.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/prop-types */
+import JackPotImg from "../assets/jackpot.png";
+import JackPotImg2 from "../assets/jackpot2.png";
+
+export default function JackPot({ p, time, title, second }) {
+  const styles = {
+    jackpot: "relative md:mb-60 mb-[300px]",
+    jackpotImg: `${second ? "md:h-[550px]" : "md:h-[450px]"} ${
+      !second && "h-[295px]"
+    } w-auto
+    }`,
+    h1: "text-[2.5em] text-center mb-7 parisienne",
+    mainContent:
+      "flex flex-col flex-wrap md:flex-nowrap justify-center items-center w-full py-5 absolute bottom-[-250px] md:bottom-[-200px] gap-2 rounded-3xl bg-[#3232328F]",
+    morphism:
+      "bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-5",
+    p: "text-[0.8em] mx-auto w-[81%] text-center",
+    green: "text-[color:var(--light-green)] underline",
+    h2: "text-[2.2em] font-bold",
+    h3: "text-[1.7em] font-bold",
+    blue: "text-[#6BD3FA] font-bold text-[1]",
+  };
+  return (
+    <div className={styles.jackpot}>
+      <h1 className={styles.h1}>{title}</h1>
+      <img
+        className={styles.jackpotImg}
+        src={second ? JackPotImg2 : JackPotImg}
+        alt=""
+      />
+      <div className={styles.mainContent}>
+        <h3 className={styles.h3}>Jackpot</h3>
+        {time && <p className={styles.p + " " + styles.blue}>{time}</p>}
+        <h2
+          className={styles.h2}
+          style={{
+            background:
+              "linear-gradient(90deg, #B24FAB 2.41%, #D85388 106.17%)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            backgroundClip: "text",
+          }}
+        >
+          8,403 USDC
+        </h2>
+        <p className={styles.p}>
+          {p}
+          {/* This is guaranteed by a{" "}
+          <span className={styles.green}>smart contract</span>. */}
+        </p>
+        <p className={`${styles.p} ${styles.green}`}>
+          Last wallet Address: <br /> 0x0000.0000
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/homepage/Stake.jsx
@@ -0,0 +1,679 @@
+import { BiChevronDown } from "react-icons/bi";
+import { React, useState, useEffect } from "react";
+import networks from "./networks.jsx";
+const {ethers, BigNumber}  = require("ethers");
+const provider = new ethers.providers.JsonRpcProvider(networks.sepolia.rpcUrl);
+
+const styles = {
+  stake: "w-full p-9 rounded-3xl relative",
+  h1: "text-[1.8em]",
+  total: "ttext-[0.6em] absolute top-5 right-7",
+  desc: "text-[1.2em] mt-2",
+  pill: "bg-[#1F1F1F] w-[100%] md:width-[initial] border border-white text-white text-center rounded-full px-5 py-2 text-[1.1em]",
+  pills:
+    "flex md:flex-row flex-col flex-wrap md:flex-nowrap justify-center items-center gap-7 md:gap-16 mt-10",
+};
+
+const stakingcontractAddress = "0x4ddd7b7adC3Cd1A16b7b2c5a8EB0E6A20B52Bb39";
+const tokenContractAddress = "0x1e181E387786D7a3d18520fE335aCDF97C3519d8";
+const stakingcontractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_minimumDurationInDays",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_batman",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "winner",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "JackpotWon",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "Received",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "staker",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "Staked",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "staker",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "Unstaked",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "activateRaffle",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "batman",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getJackpotBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "minimumDuration",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "stake",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "stakers",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "stakedAmount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "unstake",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	}
+]
+const tokenContractABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "DOMAIN_SEPARATOR",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "subtractedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "decreaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "addedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "increaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "mint",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "nonces",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "deadline",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint8",
+				"name": "v",
+				"type": "uint8"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "r",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "s",
+				"type": "bytes32"
+			}
+		],
+		"name": "permit",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+
+export default function Stake() {
+  const [stakeAmount, setStakeAmount] = useState("");
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  provider.send("eth_requestAccounts", []);
+
+  const signer = provider.getSigner();
+
+  const handleInputChange = (event) => {
+    setStakeAmount(event.target.value);
+  };
+
+  const approve = async (amount) => {
+	try {
+	  const tokenContract = new ethers.Contract(tokenContractAddress, tokenContractABI, signer);
+	  const allowance = await tokenContract.allowance(signer.getAddress(), stakingcontractAddress);
+	  
+	  // Check if the allowance is sufficient, if not, approve the staking contract
+	  if (allowance.lt(amount)) {
+		const approveTx = await tokenContract.approve(stakingcontractAddress, amount);
+		await approveTx.wait();
+		console.log("Approved successfully!");
+		alert("Approved successfully!");
+	  } else {
+		console.log("Already approved!");
+	  }
+	} catch (error) {
+	  console.error("Error approving:", error);
+	  // Handle the error as needed
+	}
+  };
+  
+
+  const handleStake = async () => {
+    try {
+
+      const contract = new ethers.Contract(
+        stakingcontractAddress,
+        stakingcontractABI,
+        signer
+      );
+
+      const amount = ethers.utils.parseUnits(stakeAmount, "wei");
+	 // const amount = BigNumber.from(stakeAmount).mul(BigNumber.from(10).pow(18));
+	  await approve(amount);
+	  const gasLimit = 30000000; 
+
+      const tx = await contract.stake(amount, { gasLimit });
+      await tx.wait();
+
+      console.log("Staked successfully!");
+      alert("Staked successfully!");
+      // Add any additional logic or UI updates here
+    } catch (error) {
+      console.error("Error staking:", error);
+	  const errorMessage = `Staking Failed! Error: ${error.message}`; // Get the error message
+      alert(errorMessage);
+      // Handle the error as needed
+    }
+  };
+
+  return (
+    <form
+      className={styles.stake}
+      style={{ background: "var(--component-gradient-bg)" }}
+    >
+      <p className={styles.total}>Total Staked: 473,492 $777</p>
+      <h1 className={styles.h1}>Stake</h1>
+      <p className={styles.desc}>
+        Stake to be entered into the weekly giveaway.
+      </p>
+      <div className={styles.pills}>
+        <input
+          type="text"
+          className={styles.pill}
+          placeholder="Amount"
+          onChange={handleInputChange}
+          value={stakeAmount}
+        />
+        <button
+          className={styles.pill + " bg-[#000000] font-bold"}
+          onClick={handleStake}
+        >
+          Stake
+        </button>
+      </div>
+    </form>
+  );
+}
